868422964ed175b49aec651739cacaddd0f1fa7d
Bhumika <Bhumikach04@gmail.com>
2026-08-06 18:24:53 +0530

    connected the project to the github

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+    <p className="text-pink-500 font-bold text-xl bg-amber-300">Hello world!</p>
+    </>
+  )
+}
+
+export default App
